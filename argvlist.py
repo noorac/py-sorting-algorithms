45a00625910bc4f7ad31af671989a9d79eb878ad
Kjetil Paulsen <kjetil_paulsen@hotmail.com>
2024-10-24 10:04:11 +0200

    added argvlist and updated bubblesort main

---
+++ argvlist.py
@@ -0,0 +1,27 @@
+# Argvlist, a script that turns a list of argv arguments into a list of integers
+
+
+def argvlist(listtoconvert) -> list:
+    """
+    Makes an attempt at turning the elements of a list into
+    integers. In case of ValueError, exit the program
+    """
+    for i in range(len(listtoconvert)):
+        try:
+            listtoconvert[i] = int(listtoconvert[i])
+        except ValueError:
+            print(f"ValueError: Check arguments")
+            exit()
+    return listtoconvert
+
+
+def main() -> None:
+    from sys import argv
+
+    listtosort = argvlist(argv[1:])
+    print(f"Converted list:{listtosort}")
+    return None
+
+
+if __name__ == "__main__":
+    main()
